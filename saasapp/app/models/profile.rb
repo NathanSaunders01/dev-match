07f3596e62e2f626979111f7572263296981682b
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-03-12 16:54:59 +0000

    Added file field for image upload to profile

--- saasapp/app/models/profile.rb
+++ saasapp/app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   # EXAMPLE: has_many :images (see example in profile controller)
   has_attached_file :avatar, 
-                    :styles { :medium "300x300>", :thumb "100x100>" }, 
-                    :default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\z/
 end
