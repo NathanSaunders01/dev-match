aaed3be41d0ef2391af8e1620255073ed353ab1c
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-03-12 13:46:09 +0000

    Added user profiles form

--- saasapp/app/controllers/profiles_controller.rb
+++ saasapp/app/controllers/profiles_controller.rb
@@ -1,7 +1,10 @@
 class ProfilesController < ApplicationController
+  # Define instance variables with @ in controller and then reference them in views files
+  # Must have same def/view file name for it to pick. Example def new and views->profiles->new
   
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
 end
