0a827e4c6c737756d6c559a123abdba1a6f423a7
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-03-21 16:10:55 +0000

    Added Position column to League Tables

--- saasapp/app/controllers/leagues_controller.rb
+++ saasapp/app/controllers/leagues_controller.rb
@@ -1,6 +1,8 @@
 class LeaguesController < ApplicationController
   def index
-    @LMSusers = User.all.order('last_sign_in_at DESC')
-    @MVPusers = User.all.order('sign_in_count DESC')
+    @LMSusers = User.order('last_sign_in_at DESC')
+    @MVPusers = User.limit(5).order('sign_in_count DESC')
+    @LMSuser_position = User.order('last_sign_in_at DESC').find_index(current_user)
+    @MVPuser_position = User.order('sign_in_count DESC').find_index(current_user)
   end
 end
